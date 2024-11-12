1f46bb1e579c153efb22d04e21f1b0c701195360
whitedmason <masonwhited@byui.edu>
2024-11-12 15:32:08 -0700

    create game folder

---
+++ game/target/debug/deps/game.d
@@ -0,0 +1,5 @@
+C:\Users\mason\hangman_game\game\target\debug\deps\game.exe: src/main.rs
+
+C:\Users\mason\hangman_game\game\target\debug\deps\game.d: src/main.rs
+
+src/main.rs:
---
+++ game/target/debug/game.d
@@ -0,0 +1,1 @@
+C:\Users\mason\hangman_game\game\target\debug\game.exe: C:\Users\mason\hangman_game\game\src\main.rs
